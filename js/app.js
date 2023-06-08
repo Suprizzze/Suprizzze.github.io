27c2870ae58b1bee235bb2afba4f4b6501e9de30
Gegham <126511969+Suprizzze@users.noreply.github.com>
2023-06-08 23:16:36 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// 3D Scroll
+
+let zSpacing = -1000,
+		lastPos = zSpacing / 5,
+		$frames = document.getElementsByClassName('frame'),
+		frames = Array.from($frames),
+		zVals = []
+
+window.onscroll = function() {
+
+	let top = document.documentElement.scrollTop,
+			delta = lastPos - top 
+
+	lastPos = top
+
+	frames.forEach(function(n, i) {
+		zVals.push((i * zSpacing) + zSpacing) - 300
+		zVals[i] += delta * -9
+		let frame = frames[i],
+				transform = `translateZ(${zVals[i]}px)`,
+				opacity = zVals[i] < Math.abs(zSpacing) / 1.2 ? 1 : 0
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+	})
+
+}
+
+window.scrollTo(0, 1)
+
+// Audio
+
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+sou = addEventListener("scroll", (event) => {audio.play()});
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
+
+
+const sliderMain = new Swiper('.slider_main', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+})
+const sliderBg = new Swiper('.slider_bg', {
+	centeredSlides: true,
+	parallax: true,
+	spaceBetween: 60,
+	slidesPerView: 5
+})
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
+})
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
